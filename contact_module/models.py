8b07c780e002debead014655f2ef44482a0f0802
nimagolkhanban <ningoban@gmail.com>
2023-11-14 21:24:49 +0330

    adding some detail to contact us module

--- contact_module/models.py
+++ contact_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    title = models.CharField(max_length=300, verbose_name="عنوان")
+    email = models.EmailField(max_length=300, verbose_name="ایمیل")
+    full_name = models.CharField(max_length=300, verbose_name="متن تماس با ما")
+    message = models.TextField(verbose_name="متن پیام ارسالی")
+
+    class Meta:
+        verbose_name = "تماس با ما"
+        verbose_name_plural = "لیست تماس با ما"
+
+    def __str__(self):
+        return self.title
